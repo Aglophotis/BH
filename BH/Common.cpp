ab52bbef77184b563a3fe37effe605688f6c7cfc
Aglophotis <Atisphatis@gmail.com>
2023-12-30 00:11:10 +0300

    Add locale for keys

--- BH/Common.cpp
+++ BH/Common.cpp
@@ -206,7 +206,7 @@ void PrintText(DWORD Color, char* szText, ...) {
 }
 
 KeyCode pCodes[] = {
-	{"None", 0, "Not Set"},
+	{"None", 0, "Not Set", "key.none"},
 	{"VK_BACK", 0x08, "Backspace"},
 	{"VK_TAB", 0x09, "Tab"},
 	{"VK_CLEAR", 0x0C, "Clear"},
